2ef5d240aeabb2699fbe713174bb9790bc568cc3
Charlie.com <csru22005@g.nihon-u.ac.jp>
2023-03-30 02:50:41 +0000

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include "mbed.h"
 
-DigitalOut myled(LED1);
+RawSerial pc(USBTX, USBRX);
+DigitalOut led0(LED1), led25(LED2), led50(LED3), led75(LED4);
 
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
+AnalogIn temt6000(p15);
+// temt6000 breakout: VCC(to VOUT=3.3V), GND(to GND), SIG(to p15) 
+
+int main()
+{
+  for(;;)
+  {
+    float x = temt6000;
+    
+    // four LEDs meaning "light level" (1 to 4):
+    led0 = 1;
+    led25 = led50 = led75 = 0;
+    if(x>0.25) led25 = 1;
+    if(x>0.50) led50 = 1;
+    if(x>0.75) led75 = 1;
+    
+    printf("%f\r\n", x);
+    wait(0.50);
   }
 }
